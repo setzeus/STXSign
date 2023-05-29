b658c67b4c1eabb495d33b2038e91dadd6744295
jesus <jnajera1917@gmail.com>
2023-05-29 18:05:51 -0400

    Updated icons & started rest of view

--- app/src/main/java/com/example/stxsign/SignScreen.kt
+++ app/src/main/java/com/example/stxsign/SignScreen.kt
@@ -24,9 +24,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,39 +46,48 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun SignScreen(navController: NavHostController, navBackStackEntry: NavBackStackEntry?, currentRoute: String?) {
+
+    val requestsText = AnnotatedString.Builder().apply {
+        withStyle(
+            style = SpanStyle(textDecoration = TextDecoration.Underline)
+        ) {
+            append("2")
+        }
+    }.toAnnotatedString()
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        Column() {
-            Text("2 Pending Reqs")
+        Column( modifier = Modifier.padding(horizontal = 16.dp).padding(top = 16.dp)) {
             Row() {
-                Text("Sort by")
-                Text("time to expire")
-                Icon(
-                    Icons.Default.KeyboardArrowLeft,
-                    contentDescription = "open",
-                    modifier = Modifier.size(32.dp),
+                Text(text = requestsText, fontSize = 36.sp, fontWeight = FontWeight.ExtraBold, modifier = Modifier)
+                Text(text = " Pending Reqs", fontSize = 36.sp, fontWeight = FontWeight.Light, modifier = Modifier)
+            }
+            Row() {
+                Text("sort by", fontWeight = FontWeight.ExtraLight, fontSize = 16.sp)
+                Text(" time to expire", fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                Image(
+                    painter = painterResource(id = R.drawable.filtericonss),
+                    contentDescription = "Restfully Placeholder Coach Image",
+                    modifier = Modifier.size(20.dp),
+                    contentScale = ContentScale.Fit
                 )
                 Spacer(Modifier.weight(1f))
-                Button(onClick = { /*TODO*/ }) {
-                    Icon(
-                        Icons.Default.KeyboardArrowLeft,
-                        contentDescription = "open",
-                        modifier = Modifier.size(32.dp),
-                    )
-                }
-                Button(onClick = { /*TODO*/ }) {
-                    Icon(
-                        Icons.Default.KeyboardArrowLeft,
-                        contentDescription = "open",
-                        modifier = Modifier.size(32.dp),
-                    )
-                }
+                Image(
+                    painter = painterResource(id = R.drawable.listiconss),
+                    contentDescription = "Restfully Placeholder Coach Image",
+                    modifier = Modifier.size(20.dp),
+                    contentScale = ContentScale.Fit
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.gridiconss),
+                    contentDescription = "Restfully Placeholder Coach Image",
+                    modifier = Modifier.size(20.dp),
+                    contentScale = ContentScale.Fit
+                )
             }
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)) {
+            Row(modifier = Modifier.fillMaxWidth()) {
                 // sbtc or btc logo
                 Image(
                     painter = painterResource(id = R.drawable.sbtclogo),
@@ -121,11 +136,17 @@ fun ClearButton(onClick: () -> Unit) {
         },
         modifier = Modifier.rotate(if (rotated) -90f else 0f)
     ) {
-        Icon(
-            Icons.Default.KeyboardArrowLeft,
-            contentDescription = "open",
-            modifier = Modifier.size(32.dp),
+        Image(
+            painter = painterResource(id = R.drawable.chevlefticonss),
+            contentDescription = "Restfully Placeholder Coach Image",
+            modifier = Modifier.size(20.dp),
+            contentScale = ContentScale.Fit
         )
+//        Icon(
+//            Icons.Default.KeyboardArrowLeft,
+//            contentDescription = "open",
+//            modifier = Modifier.size(32.dp),
+//        )
     }
 }
 

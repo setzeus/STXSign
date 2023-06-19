60a233b2ec4743183dff1614ccb39073ba87120f
jesus <jnajera1917@gmail.com>
2023-06-18 21:05:35 -0400

    Auto deny list component

--- app/src/main/java/com/example/stxsign/StackScreen.kt
+++ app/src/main/java/com/example/stxsign/StackScreen.kt
@@ -1,13 +1,12 @@
 package com.example.stxsign
+import android.graphics.ColorFilter
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -31,6 +30,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter.Companion.tint
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontStyle
@@ -46,12 +46,17 @@ fun StackScreen(navController: NavHostController, navBackStackEntry: NavBackStac
     var privateHashedKey by remember { mutableStateOf("************************************************************") }
     var publicSigningKey by remember { mutableStateOf("0377c011d562bb0203bf5adbd1c9325bbf19d7085fe80b23d30bf9f390b0fc32d8") }
     var currentNetwork by remember { mutableStateOf("Mainnet") }
+    var newDenyAddress by remember { mutableStateOf("03........") }
 
     var hasChanges by remember { mutableStateOf(false) }
+
+    var showAutoDenyAdd by remember { mutableStateOf(false) }
 
     var sliderValues by remember {
         mutableStateOf(.000000001f..100f)
     }
+
+    var autoDenyAddresses = remember { mutableStateListOf<String>() }
 
     val keyboardController = LocalSoftwareKeyboardController.current
 
@@ -67,7 +72,9 @@ fun StackScreen(navController: NavHostController, navBackStackEntry: NavBackStac
                 alpha = 0.5f
             )) {
             Column(modifier = Modifier
-                .padding(horizontal = 12.dp),
+                .padding(horizontal = 12.dp)
+                .padding(bottom = 48.dp)
+                .verticalScroll(state = ScrollState(initial = 0)),
                 verticalArrangement = Arrangement.Top,
             ) {
 
@@ -164,7 +171,7 @@ fun StackScreen(navController: NavHostController, navBackStackEntry: NavBackStac
                     label = { Text("Current Network") },
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(bottom = 16.dp)
+                        .padding(bottom = 20.dp)
                 )
                 Text(text = "Approval Settings", fontSize = 24.sp, fontWeight = FontWeight.Black, modifier = Modifier)
                 Text(text = "This second batch of settings configure what transactions (if any), to autosign.", fontSize = 16.sp, fontWeight = FontWeight.Light, modifier = Modifier
@@ -192,7 +199,9 @@ fun StackScreen(navController: NavHostController, navBackStackEntry: NavBackStac
                     thumbColor = colorResource(id = R.color.gray_button_400)
                 )
                     )
-                Row(modifier = Modifier.fillMaxWidth().padding(bottom = 12.dp)) {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)) {
                     Text(text = "%.9f".format(sliderValues.start))
                     Spacer(Modifier.weight(1f))
                     Text(text = "%.9f".format(sliderValues.endInclusive))
@@ -200,6 +209,37 @@ fun StackScreen(navController: NavHostController, navBackStackEntry: NavBackStac
                 Row(modifier = Modifier.fillMaxWidth()) {
                     Text(text = "Auto-Deny List", fontSize = 16.sp, fontWeight = FontWeight.SemiBold, modifier = Modifier)
                     Text(text = "(addresses)", fontSize = 16.sp, fontWeight = FontWeight.Light, modifier = Modifier.padding(start = 4.dp))
+                    Spacer(Modifier.weight(1f))
+                    if (showAutoDenyAdd) {
+                        Box(modifier = Modifier
+                            .size(32.dp)
+                            .clickable {
+                                autoDenyAddresses.add(newDenyAddress)
+                            }
+                            .background(colorResource(id = R.color.gray_button_400), shape = CircleShape), contentAlignment = Alignment.Center) {
+                            Text("+", fontWeight = FontWeight.Bold, color = Color.White)
+                        }
+                    } else {
+                        Box(modifier = Modifier
+                            .size(32.dp)
+                            .background(colorResource(id = R.color.gray_button_400).copy(alpha = 0.25f), shape = CircleShape), contentAlignment = Alignment.Center) {
+                            Text("+", fontWeight = FontWeight.Bold, color = Color.White)
+                        }
+                    }
+                }
+                autoDenyAddresses.forEach { address ->
+                    Text(text = address)
+                }
+                Row(modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp)) {
+                    OutlinedTextField(
+                        value = newDenyAddress,
+                        onValueChange = { newDenyAddress = it
+                            hasChanges = true
+                                        showAutoDenyAdd = true
+                                        },
+                        label = { Text("New reject address") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
                 }
 
                 Text(text = "Demo Settings", fontSize = 24.sp, fontWeight = FontWeight.Black, modifier = Modifier)
@@ -209,4 +249,6 @@ fun StackScreen(navController: NavHostController, navBackStackEntry: NavBackStac
             }
         }
     }
+
 }
+
